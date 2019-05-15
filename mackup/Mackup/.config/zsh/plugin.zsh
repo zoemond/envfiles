12829cf56fdfac817585f0d1dee6ca56773e3d19
koki <myaddressisthis123@gmail.com>
2019-05-15 17:49:04 +0900

    feat: ranger, zsh: ranger-cdの設定など記述

--- mackup/Mackup/.config/zsh/plugin.zsh
+++ mackup/Mackup/.config/zsh/plugin.zsh
@@ -3,7 +3,23 @@ source /usr/local/share/autojump/autojump.zsh
   
 # rangerの多重起動を避ける 
 ranger() { [ -n "$RANGER_LEVEL" ] && exit || LESS="$LESS -+F -+X" command ranger "$@"; }
+
+# 多重起動中はpromptに(RANGER)だす
 [ -n "$RANGER_LEVEL" ] && PS1="(RANGER) $PS1" 
+
+function ranger-cd {
+    tempfile="$(mktemp -t tmp.XXXXXX)"
+    /usr/local/bin/ranger --choosedir="$tempfile" "${@:-$(pwd)}"
+    test -f "$tempfile" &&
+    if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
+        cd -- "$(cat "$tempfile")"
+    fi  
+    rm -f -- "$tempfile"
+}
+
+
+#ranger-cd will fire for Ctrl+O
+bindkey -s '^O' 'ranger-cd\n'
 
 # -----------------------------
 # ZPLUG
