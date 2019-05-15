b10d4124f9ad6017f5432aa76bba627621a88d8f
koki <myaddressisthis123@gmail.com>
2019-05-16 05:55:14 +0900

    feat: cd するのはrangerをquitするときに決めるようにした

--- mackup/Mackup/.config/zsh/plugin/ranger.zsh
+++ mackup/Mackup/.config/zsh/plugin/ranger.zsh
@@ -4,17 +4,25 @@ ranger() { [ -n "$RANGER_LEVEL" ] && exit || LESS="$LESS -+F -+X" command ranger
 # 多重起動中はpromptに(RANGER)だす
 [ -n "$RANGER_LEVEL" ] && PS1="(RANGER) $PS1" 
 
-function ranger-cd {
-    tempfile="$(mktemp -t tmp.XXXXXX)"
-    /usr/local/bin/ranger --choosedir="$tempfile" "${@:-$(pwd)}"
-    test -f "$tempfile" &&
-    if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
-        cd -- "$(cat "$tempfile")"
-    fi  
-    rm -f -- "$tempfile"
+# https://github.com/ranger/ranger/wiki/Integration-with-other-programs
+# > if you want to change the dir on demand after you exit ranger, by the following wrapper function.
+function ranger {
+    local IFS=$'\t\n'
+    local tempfile="$(mktemp -t tmp.XXXXXX)"
+    local ranger_cmd=(
+        command
+        ranger
+        --cmd="map Q chain shell echo %d > "$tempfile"; quitall"
+    )
+    
+    ${ranger_cmd[@]} "$@"
+    if [[ -f "$tempfile" ]] && [[ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]]; then
+        cd "$(cat "$tempfile")" || return
+    fi
+    command rm -f -- "$tempfile" 2>/dev/null
 }
 
 #ranger-cd will fire for Ctrl+O
-bindkey -s '^O' 'ranger-cd\n'
+bindkey -s '^O' 'ranger\n'
 
 
