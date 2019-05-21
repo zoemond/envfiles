4dc6162111a33c9f1d744fd3a29ef5e0a5f363aa
koki <myaddressisthis123@gmail.com>
2019-05-21 22:20:26 +0900

    feat: promptの色を変えたときとか、すぐ反映されるように

--- mackup/Mackup/.config/fish/myconf/fish_right_prompt.fish
+++ mackup/Mackup/.config/fish/myconf/fish_right_prompt.fish
@@ -1,33 +1,15 @@
 
 set -x fish_prompt_pwd_dir_length 0
 function fish_right_prompt 
-  if not set -q __fish_git_prompt_show_informative_status
-    set -g __fish_git_prompt_show_informative_status 1
-  end
-  if not set -q __fish_git_prompt_color_branch
-    set -g __fish_git_prompt_color_branch brmagenta
-  end
-  if not set -q __fish_git_prompt_showupstream
-    set -g __fish_git_prompt_showupstream "informative"
-  end
-  if not set -q __fish_git_prompt_showdirtystate
-    set -g __fish_git_prompt_showdirtystate "yes"
-  end
-  if not set -q __fish_git_prompt_color_stagedstate
-    set -g __fish_git_prompt_color_stagedstate green
-  end
-  if not set -q __fish_git_prompt_color_invalidstate
-    set -g __fish_git_prompt_color_invalidstate red
-  end
-  if not set -q __fish_git_prompt_color_dirtystate
-    set -g __fish_git_prompt_color_dirtystate red
-  end
-  if not set -q __fish_git_prompt_color_cleanstate
-    set -g __fish_git_prompt_color_cleanstate brgreen
-  end
-  if not set -q __fish_git_prompt_color_untrackedfiles
-    set -g __fish_git_prompt_color_untrackedfiles yellow
-  end
+  set -g __fish_git_prompt_show_informative_status 1
+  set -g __fish_git_prompt_color_branch black 
+  set -g __fish_git_prompt_showupstream "informative"
+  set -g __fish_git_prompt_showdirtystate "yes"
+  set -g __fish_git_prompt_color_stagedstate green
+  set -g __fish_git_prompt_color_invalidstate red
+  set -g __fish_git_prompt_color_dirtystate red
+  set -g __fish_git_prompt_color_cleanstate brgreen
+  set -g __fish_git_prompt_color_untrackedfiles yellow
 
   printf '%s%s%s%s ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (__fish_git_prompt) 
 end
