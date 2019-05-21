2d46c56a501e86080439be041c33bcd286d97c43
koki <ip.b086.koki.shinichi@gmail.com>
2019-05-21 15:44:56 +0900

    feat: ip用のファイルを作成

---
+++ mackup/Mackup/.config/fish/myconf/ip.fish
@@ -0,0 +1,8 @@
+set PATH /Users/shinichikoki/Library/Android/sdk/platform-tools $PATH 
+set PATH ~/.nodenv/shims/ $PATH 
+set -x JAVA_HOME (/usr/libexec/java_home -v "1.8")
+set -x PATH $JAVA_HOME/bin:$PATH
+
+# anyenv入れるのむずい
+# anyenv init - | source
+
