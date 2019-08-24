80af1d9047dc1c39f0e7c3c8ac0078b7ee1e27c8
koki <myaddressisthis123@gmail.com>
2019-08-24 13:28:56 +0900

    copyq: script: to camel case

---
+++ mackup/Mackup/.config/copyq/toCamelCase.js
@@ -0,0 +1,32 @@
+
+function reduceJoinChar(str, preCurrCharFunc) {
+	var appendedStr = ''
+	var beforeChar = '';
+	for (var i = 0; i < str.length; i ++) {
+		var currentChar = str.charAt(i); 
+
+		var replaced = preCurrCharFunc(beforeChar, currentChar);
+		appendedStr = appendedStr + replaced;
+
+		beforeChar = currentChar;
+	}
+	return appendedStr;
+}
+
+function isSeparator(chara) {
+	if (chara === '-') return true;
+	if (chara === '_') return true;
+	if (chara === ' ') return true;
+	return false;
+}
+
+function toCamel(beforeChar, currentChar){
+	if (isSeparator(currentChar)) return '';
+	if (isSeparator(beforeChar)) return currentChar.toUpperCase();
+	return currentChar;
+}
+
+var cliped = str(read(0));
+var appended = reduceJoinChar(cliped, toCamel);
+
+print(appended);
