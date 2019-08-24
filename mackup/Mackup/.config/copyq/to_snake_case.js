0380a463ced75688ddc59d2b70875db90226fb84
koki <myaddressisthis123@gmail.com>
2019-08-24 14:21:33 +0900

    copyq: script: to snake case

---
+++ mackup/Mackup/.config/copyq/to_snake_case.js
@@ -0,0 +1,46 @@
+
+function reduceJoinChar(str, preCurrCharFunc) {
+	var appendedStr = ''
+	var beforeChar = '';
+	for (var i = 0; i < str.length; i ++) {
+		var currentChar = str.charAt(i); 
+
+		var replaced = preCurrCharFunc(beforeChar, currentChar);
+		appendedStr = appendedStr + replaced;
+
+		beforeChar = currentChar;
+	}
+	return appendedStr;
+}
+
+function isUpperCase(chara) {
+	return chara === chara.toUpperCase();
+}
+
+function isSeparator(chara) {
+	if (chara === '-') return true;
+	if (chara === '_') return true;
+	if (chara === ' ') return true;
+	return false;
+}
+
+function toSnake(beforeChar, currentChar) {
+	if (isSeparator(currentChar)) {
+		if (isSeparator(beforeChar)) { // 連続するseparatorをひとまとめにする
+			return '';
+		}
+		return '_';
+	}
+
+	var lowChar = currentChar.toLowerCase();
+
+	if (isUpperCase(beforeChar)) return lowChar;
+	if (isUpperCase(currentChar)) return '_' + lowChar;
+
+	return lowChar;
+}
+
+var cliped = str(read(0));
+var appended = reduceJoinChar(cliped, toSnake);
+
+print(appended);
