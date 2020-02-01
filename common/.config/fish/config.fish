9a8f9d09eb22f20e1001627bb4f7320fb4a436a2
koki <myaddressisthis123@gmail.com>
2020-02-01 19:48:17 +0900

    feat: ファイルサイズを見やすく

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -5,7 +5,7 @@ set -x EDITOR nvim
 abbr vi "nvim"
 abbr rm "trash-put"
 abbr ls "ls -aG"
-abbr ll "ls -l"
+abbr ll "ls -lh"
 
 set variable (dirname ( status --current-filename ))
 for f in (find $variable/myconf/* -type f -name '*.fish')
