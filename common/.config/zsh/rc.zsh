a821c49513092f7ecdbfea995c591a404a4cbeb6
koki <dummy@dummy.co>
2020-03-03 17:50:24 +0900

    zsh: brew cask install android-sdk

--- common/.config/zsh/rc.zsh
+++ common/.config/zsh/rc.zsh
@@ -3,3 +3,4 @@ eval "$(nodenv init -)"
 export PATH="$HOME/.pyenv/shims:$PATH"
 eval "$(nodenv init -)"
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+export ANDROID_HOME=/usr/local/share/android-sdk
