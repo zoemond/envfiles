f5c6e7694fb5435582121dc2d11dd5be5ab5eedd
koki <dummy@dummy.co>
2020-03-03 18:21:44 +0900

    android

--- common/.config/zsh/rc.zsh
+++ common/.config/zsh/rc.zsh
@@ -4,3 +4,4 @@ export PATH="$HOME/.pyenv/shims:$PATH"
 eval "$(nodenv init -)"
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 export ANDROID_HOME=/usr/local/share/android-sdk
+export ANDROID_SDK_ROOT=/usr/local/share/android-sdk
