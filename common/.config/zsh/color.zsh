8c33039a80d5f95fc90531a8e413a6c3ba3521b7
koki <dummy@dummy>
2020-04-01 21:54:53 +0900

    zsh(color): grep color

--- common/.config/zsh/color.zsh
+++ common/.config/zsh/color.zsh
@@ -1,9 +1,11 @@
 # ls時の色
 export LSCOLORS=Exfxcxdxbxegedabagacad
 
-
 # 補完時の色. ls時の色と合わす
 export LS_COLORS='di=01;34:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 
 # 補完候補に色つける
 zstyle ':completion:*' list-colors "${LS_COLORS}" 
+
+export GREP_OPTIONS='--color=always'
+
