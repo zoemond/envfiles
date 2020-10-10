b41db475b85f4f5a1b010c015433b7df88b841de
zoemond <dummy@example.com>
2020-10-11 00:14:56 +0900

    tmux: wsl2でコピーする

--- common/.config/zsh/plugin/install.zsh
+++ common/.config/zsh/plugin/install.zsh
@@ -4,24 +4,24 @@
 
 ## defer: 読み込み順序を設定する
 # 例: "zsh-syntax-highlighting" は compinit の後に読み込まれる必要がある
-# zplug "zsh-users/zsh-syntax-highlighting", defer:2 
+# zplug "zsh-users/zsh-syntax-highlighting", defer:2
 # （2 以上は compinit 後に読み込まれるようになる）
 #####
 
-# zplugが無ければインストール 
-if [[ ! -d ~/.zplug ]];then 
+# zplugが無ければインストール
+if [[ ! -d ~/.zplug ]];then
   echo 'zplug is not installed. install'
   curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 fi
 
 # zplugを有効化する
-source ~/.zplug/init.zsh 
+source ~/.zplug/init.zsh
 
-zplug "zsh-users/zsh-autosuggestions" 
+zplug "zsh-users/zsh-autosuggestions"
 
-zplug "zsh-users/zsh-syntax-highlighting", defer:2 
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
-zplug "zsh-users/zsh-history-substring-search" 
+zplug "zsh-users/zsh-history-substring-search"
 if zplug check "zsh-users/zsh-history-substring-search"; then
     bindkey '^P' history-substring-search-up
     bindkey '^N' history-substring-search-down
@@ -29,7 +29,7 @@ fi
 
 zplug "b4b4r07/enhancd", use:init.sh
 
-zplug "momo-lab/zsh-abbrev-alias" 
+zplug "momo-lab/zsh-abbrev-alias"
 
 zplug romkatv/powerlevel10k, as:theme, depth:1
 
@@ -37,7 +37,7 @@ zplug romkatv/powerlevel10k, as:theme, depth:1
 zplug "b4b4r07/zsh-gomi", if:"which fzf"
 
 # -----------------------------
-# インストールしていないプラグインをインストール 
+# インストールしていないプラグインをインストール
 # -----------------------------
 if ! zplug check --verbose; then
   printf "Install? [y/N]: "
