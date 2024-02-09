e4cae2a7845283a3f69f30ac049c413ba967bded
zoemond <dummy@dummy.com>
2024-02-09 18:58:46 +0900

    wezterm: config

---
+++ common/.config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local keyConfig = require 'keybinds'
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+-- local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+
+-- and finally, return the configuration to wezterm
+return keyConfig
